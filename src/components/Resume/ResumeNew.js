f93ff9ca3be5d23382f33b8f6147166f99d40c55
shahraim <shahraim.khan27112002@gmail.com>
2023-07-09 15:13:43 +0500

    resume change

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import MyResume from "../../Assets/MyResume.pdf";
+import MyResume from "../../Assets/MSKN-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
